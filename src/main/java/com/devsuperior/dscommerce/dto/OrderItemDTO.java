25dfc394e414b77c8ff2c4bf792519fc811671b3
lianasabino <lianasabino@hotmail.com>
2025-02-12 13:48:40 -0300

    Validate self or admin

--- src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
+++ src/main/java/com/devsuperior/dscommerce/dto/OrderItemDTO.java
@@ -8,15 +8,18 @@ public class OrderItemDTO {
 	private String name;
 	private Double price;
 	private Integer quantity;
+	private String imgUrl;
 	
 	public OrderItemDTO() {
 	}
 	
-	public OrderItemDTO(Long productId, String name, Double price, Integer quantity) {
+	public OrderItemDTO(Long productId, String name, Double price, Integer quantity, String imgUrl) {
 		this.productId = productId;
 		this.name = name;
 		this.price = price;
 		this.quantity = quantity;
+		this.imgUrl = imgUrl;
+		
 	}
 	
 	public OrderItemDTO(OrderItem entity) {
@@ -24,6 +27,7 @@ public class OrderItemDTO {
 		name = entity.getProduct().getName();
 		price = entity.getPrice();
 		quantity = entity.getQuantity();
+		imgUrl = entity.getProduct().getImgUrl();
 	}
 
 	public Long getProductId() {
@@ -46,4 +50,7 @@ public class OrderItemDTO {
 		return price * quantity;
 	}
 	
+	public String getImgUrl() {
+		return imgUrl;
+	}
 }
--- src/main/java/com/devsuperior/dscommerce/services/OrderService.java
+++ src/main/java/com/devsuperior/dscommerce/services/OrderService.java
@@ -34,9 +34,14 @@ public class OrderService {
 	@Autowired
 	private OrderItemRepository OrderItemRepository;
 	
+	@Autowired
+	private AuthService authService;
+	
 	@Transactional(readOnly = true)
 	public OrderDTO findById(Long id) {
 		Order order = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso nao encontrado"));
+		authService.validateSelfOrAdmin(order.getClient().getId());
+		
 		return new OrderDTO(order);
 	}
 	
